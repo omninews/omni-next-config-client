72727db535bcf6201dc474bdae7db2369976d300
Alex Robsahm <alex.robsahm@athega.se>
2016-11-30 15:49:47 +0100

    Changed name and usig less packages

--- app/config/index.js
+++
@@ -1,4 +0,0 @@
-/* eslint no-process-env: 0 */
-import varium from "varium";
-
-module.exports = varium(process.env, "env.manifest");
--- index.js
+++ index.js
@@ -1,2 +1,47 @@
-require("babel-register");
-require("./app");
+const ws = require("ws");
+const url = require("url");
+const config = require("./config");
+
+const requestConfig = (webSocket, environment) => {
+  webSocket.send(environment);
+};
+
+const startUpdateConfig = (environment) => {
+  let retryNumOfTimes = 0;
+  let interval;
+
+  const connect = () => {
+    const webSocket = new ws(url.resolve(config.get("CONFIG_SERVICE_URL"), environment));
+
+    webSocket.on("open", () => {
+      retryNumOfTimes = 0;
+      requestConfig(webSocket, environment);
+      interval = setInterval(
+        requestConfig,
+        config.get("INTERVAL"),
+        webSocket,
+        environment
+      );
+    });
+
+    webSocket.on("message", (message) => {
+      process.env = Object.assign(process.env, JSON.parse(message));
+    });
+
+    webSocket.on("error", (error) => {
+      console.error(error);
+    });
+
+    webSocket.on("close", () => {
+      clearInterval(interval);
+      setTimeout(connect, Math.ceil(Math.pow(Math.E, retryNumOfTimes)) * 100);
+      retryNumOfTimes += 1;
+    });
+  };
+
+  connect();
+};
+
+module.exports = startUpdateConfig;
+
+//startUpdateConfig("dev");
